d30758b2364eb6fa63163a820b25859b215bbfca
Lucas-Assumpcaogit config --global user.email assumpdev@gmail.comgit config --global user.name Lucas-Assumpcao <assumpdev@gmail.com>
2026-05-06 21:07:02 -0300

    atualização4

--- exercoff.js
+++ exercoff.js
@@ -10,3 +10,10 @@ document.querySelectorAll('.btn').forEach((button) => {
         alert(`Você escolheu ${service}. Em breve entraremos em contato.`);
     });
 });
+
+document.querySelectorAll('.btn2').forEach((button) => {
+    button.addEventListener('click', () => {
+        const service = button.dataset.service || 'serviço';
+        alert(`Mensagem enviada! Em breve entraremos em contato.`);
+    });
+});
